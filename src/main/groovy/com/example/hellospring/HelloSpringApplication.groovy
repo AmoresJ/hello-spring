9f1e556372be1ab9ac81c3cc6dfd8b9d69e988ce
Javier Amores <javier.amores@viewnext.com>
2021-07-28 11:36:36 +0200

    Metodo REST "Hello"

--- src/main/groovy/com/example/hellospring/HelloSpringApplication.groovy
+++ src/main/groovy/com/example/hellospring/HelloSpringApplication.groovy
@@ -1,13 +1,23 @@
-package com.example.hellospring
+package com.example.hellospring;
 
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-class HelloSpringApplication {
+@RestController
+public class HelloSpringApplication {
 
-    static void main(String[] args) {
-        SpringApplication.run(HelloSpringApplication, args)
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
     }
 
 }
+
