3d20f7b77fe7e10cd430d3529b5322adb2160685
Javier Amores <javier.amores@viewnext.com>
2021-08-04 12:15:39 +0200

    Arreglo de seguridad

--- src/main/groovy/com/example/hellospring/HomeController.java
+++ src/main/groovy/com/example/hellospring/HomeController.java
@@ -1,13 +1,13 @@
 package com.example.hellospring;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/hola")
+    @GetMapping("/hola")
     public @ResponseBody String hola() {
         return "Hola holita pruebecita";
     }
