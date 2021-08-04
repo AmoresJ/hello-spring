e7351766ece59aceeca84a6643f10b2bed26f03b
Javier Amores <javier.amores@viewnext.com>
2021-08-04 11:59:57 +0200

    Corregida prueba "hola"

--- src/test/groovy/com/example/hellospring/HelloSpringApplicationTests.groovy
+++ src/test/groovy/com/example/hellospring/HelloSpringApplicationTests.groovy
@@ -3,14 +3,23 @@ package com.example.hellospring
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.web.server.LocalServerPort
+
+import static org.assertj.core.api.Assertions.assertThat
 
 @SpringBootTest
 class HelloSpringApplicationTests {
 
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
     void contextLoads() {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + this.port + "/hola", String.class)).isEqualTo("Hola holita pruebecita");
+
     }
-
-
-
 }
